89f3b78d70619d57ac06d8d862296648aab694f5
Luis Henrique Lipinski <lhlipinski@gmail.com>
2022-08-25 18:12:47 -0300

    alterado a porta

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const usersRoutes = require('./src/users/routes');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 
